c0cb28043ae8453d8434bf9f7e8889f178619b7b
VirajAdiga <virajadigamaclub@gmail.com>
2023-06-03 13:34:24 +0530

    Added search functionality

--- studymate/base/urls.py
+++ studymate/base/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+
     path("create-room/", views.create_room, name="create-room"),
+    path("rooms/<str:pk>/", views.get_room, name="get-room"),
     path("update-room/str<pk>/", views.update_room, name="update-room"),
     path("delete-room/str<pk>/", views.delete_room, name="delete-room"),
-    path("rooms/<str:pk>/", views.room, name="room")
 ]
--- studymate/base/views.py
+++ studymate/base/views.py
@@ -1,12 +1,20 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
 from base.forms import RoomForm
-from base.models import Room
+from base.models import Room, Topic
 
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, "base/home.html", {"rooms": rooms})
+    q = request.GET.get('q') if request.GET.get('q') is not None else ""
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    return render(request, "base/home.html", {"rooms": rooms, "topics": topics, "room_count": room_count})
 
 
 def create_room(request):
@@ -17,6 +25,12 @@ def create_room(request):
             form.save()
             return redirect("home")
     return render(request, "base/create_room.html", {"form": form})
+
+
+def get_room(request, pk):
+    room_requested = Room.objects.get(id=int(pk))
+    context = {"room": room_requested}
+    return render(request, "base/room.html", context)
 
 
 def update_room(request, pk):
@@ -36,9 +50,3 @@ def delete_room(request, pk):
         required_room.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": required_room})
-
-
-def room(request, pk):
-    room_requested = Room.objects.get(id=int(pk))
-    context = {"room": room_requested}
-    return render(request, "base/room.html", context)
