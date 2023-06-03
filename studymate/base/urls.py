1ea8596da98189f0c9a5fd9c688eedb120cd7c0e
VirajAdiga <virajadigamaclub@gmail.com>
2023-06-03 13:08:24 +0530

    Added CRUD functionality for room

--- studymate/base/urls.py
+++ studymate/base/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("rooms/", views.rooms, name="rooms"),
+    path("create-room/", views.create_room, name="create-room"),
+    path("update-room/str<pk>/", views.update_room, name="update-room"),
+    path("delete-room/str<pk>/", views.delete_room, name="delete-room"),
     path("rooms/<str:pk>/", views.room, name="room")
 ]
--- studymate/base/views.py
+++ studymate/base/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from base.forms import RoomForm
 from base.models import Room
 
 
 def home(request):
-    return render(request, "base/home.html")
+    rooms = Room.objects.all()
+    return render(request, "base/home.html", {"rooms": rooms})
 
 
-def rooms(request):
-    context = {"rooms": Room.objects.all()}
-    return render(request, "base/rooms.html", context)
+def create_room(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    return render(request, "base/create_room.html", {"form": form})
+
+
+def update_room(request, pk):
+    required_room = Room.objects.get(id=pk)
+    form = RoomForm(instance=required_room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=required_room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    return render(request, "base/create_room.html", {"form": form})
+
+
+def delete_room(request, pk):
+    required_room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        required_room.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": required_room})
 
 
 def room(request, pk):
